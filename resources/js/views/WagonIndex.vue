e7c58f52bd1311270fd4386a2e44b77fc7641bf7
Stoyan Haydushki <s.haidushky@abv.bg>
2020-07-07 17:54:35 +0300

    WagonIndex.vue: Design refactor
    Changed the position of the title.
    Made table to appear when there are records.
    Added .table-responsive style class to table and changed the displayed info in last column.

--- resources/js/views/WagonIndex.vue
+++ resources/js/views/WagonIndex.vue
@@ -1,43 +1,45 @@
 <template>
-  <div class="py-4">
+  <div class="py-2">
     <Search model="stylized_number" route="wagonsearch" placeholder="Търсене на вагон"></Search>
     <WagonFilter @applyFilters="applyFilters($event)"></WagonFilter>
+    <h4>Списък на всички вагони</h4>
     <div v-if="loading">Зареждане</div>
     <div v-else>
       <div v-if="wagons.length === 0">
         <p>Няма налични вагони</p>
       </div>
-      <h4>Списък на всички вагони</h4>
-      <table class="table">
-        <thead>
-          <tr>
-            <th scope="col">№</th>
-            <th scope="col">Серия</th>
-            <th scope="col">Означение</th>
-            <th scope="col">Места</th>
-            <th scope="col">Гара на домуване</th>
-            <th scope="col">Статус</th>
-            <th scope="col">Последна промяна</th>
-          </tr>
-        </thead>
-        <tbody>
-          <tr v-for="wagon in wagons">
-            <th scope="row">
-              <router-link
-                class="text-body"
-                :to="'/wagons/' + wagon.data.id"
-                :permissions="permissions"
-              >{{ wagon.data.stylized_number}}</router-link>
-            </th>
-            <td>{{wagon.data.type.data.name}}</td>
-            <td>{{wagon.data.letter_index ? wagon.data.letter_index : "-" }}</td>
-            <td>{{wagon.data.seats ? wagon.data.seats : "-"}}</td>
-            <td>{{wagon.data.depot ? wagon.data.depot.data.name : "-"}}</td>
-            <td>{{wagon.data.status.data.name}}</td>
-            <td>{{wagon.data.last_updated}}</td>
-          </tr>
-        </tbody>
-      </table>
+      <div v-else class="table-responsive text-nowrap">
+        <table class="table"> 
+          <thead>
+            <tr>
+              <th scope="col">№</th>
+              <th scope="col">Серия</th>
+              <th scope="col">Означение</th>
+              <th scope="col">Места</th>
+              <th scope="col">Гара на домуване</th>
+              <th scope="col">Статус</th>
+              <th scope="col">Изтичане на ревизия</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr v-for="wagon in wagons">
+              <th scope="row">
+                <router-link
+                  class="text-body"
+                  :to="'/wagons/' + wagon.data.id"
+                  :permissions="permissions"
+                >{{ wagon.data.stylized_number}}</router-link>
+              </th>
+              <td>{{wagon.data.type.data.name}}</td>
+              <td>{{wagon.data.letter_index ? wagon.data.letter_index : "-" }}</td>
+              <td>{{wagon.data.seats ? wagon.data.seats : "-"}}</td>
+              <td>{{wagon.data.depot ? wagon.data.depot.data.name : "-"}}</td>
+              <td>{{wagon.data.status.data.name}}</td>
+              <td>{{wagon.data.revision_expiration_date}}</td>
+            </tr>
+          </tbody>
+        </table>
+      </div>
       <Pagination :pagination="pagination" :model="model" @updatepage="updateData"></Pagination>
     </div>
   </div>
@@ -68,7 +70,7 @@ export default {
         currentPage: null,
         lastPage: null
       },
-      model: "wagons",
+      model: "wagons"
     };
   },
   methods: {
