0eccbe02de7de49228f0932e1fdf4324e8534eb1
Stoyan Haydushki <s.haidushky@abv.bg>
2020-07-19 15:49:56 +0300

    Change to order by 'id' when querying records

--- app/Http/Controllers/EventController.php
+++ app/Http/Controllers/EventController.php
@@ -22,26 +22,26 @@ class EventController extends Controller
         if (request()->has('date')) {
             $this->authorize('viewDate', Event::class);
             $date = Carbon::parse(request('date'))->toDateString();
-            $events = Event::with('wagon')->whereDate('created_at', $date)->orderByDesc('created_at')->paginate(15);
+            $events = Event::with('wagon')->whereDate('created_at', $date)->orderByDesc('id')->paginate(15);
             return EventResource::collection($events);
         }
         if (request()->has('wagon_id')) {
             $this->authorize('viewWagon', Event::class);
-            $events = Event::where('wagon_id', request('wagon_id'))->orderByDesc('created_at')->paginate(15);
+            $events = Event::where('wagon_id', request('wagon_id'))->orderByDesc('id')->paginate(15);
             return EventResource::collection($events);
         }
         if (request()->has('user_id')) {
             $this->authorize('viewUser', Event::class);
-            $events = Event::where('user_id', request('user_id'))->orderByDesc('created_at')->paginate(15);
+            $events = Event::where('user_id', request('user_id'))->orderByDesc('id')->paginate(15);
             return EventResource::collection($events);
         }
 
         $this->authorize('viewAny', Event::class);
 
-        $events = Event::orderByDesc('created_at')->paginate(15);
+        $events = Event::orderByDesc('id')->paginate(15);
 
         if (request()->has('show-wagon') && request('show-wagon')) {
-            $events = Event::with('wagon')->orderByDesc('created_at')->paginate(15);
+            $events = Event::with('wagon')->orderByDesc('id')->paginate(15);
         }
 
         return EventResource::collection($events);
