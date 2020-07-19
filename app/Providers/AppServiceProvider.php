e96240a65b35f4cac4f66eab7dd1ef381fc920b3
Stoyan Haydushki <s.haidushky@abv.bg>
2020-07-19 15:29:03 +0300

    Add DB query logging for debugging

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -7,6 +7,8 @@ use App\Observers\WagonObserver;
 use Illuminate\Support\Facades\App;
 use Illuminate\Support\Facades\Schema;
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Support\Facades\File;
+use Illuminate\Support\Facades\DB;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -33,6 +35,15 @@ class AppServiceProvider extends ServiceProvider
         if (App::environment('production')) {
             $this->app->bind('path.public', function () {
                 return base_path() . '/../public_html';
+            });
+        }
+
+        if (env('APP_DEBUG')) {
+            DB::listen(function ($query) {
+                File::append(
+                    storage_path('/logs/query.log'),
+                    $query->sql . ';' . ' #[' . implode(', ', $query->bindings) . ']' . PHP_EOL
+                );
             });
         }
     }
